718c47ae35f1dc703c0d14598593e62233db5068
OnceLearner <redaamir972@gmail.com>
2020-05-07 02:59:08 +0000

    delet some files

--- src/main/java/com/Emi/IcodeV2/controller/SubmitCodeController.java
+++ src/main/java/com/Emi/IcodeV2/controller/SubmitCodeController.java
@@ -1,6 +1,5 @@
 package com.Emi.IcodeV2.controller;
 import com.Emi.IcodeV2.service.CodeHandlerV1;
-import com.Emi.IcodeV2.service.CodeHandlerV2;
 import com.Emi.IcodeV2.service.CodeHandlerV3;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +16,7 @@ public class SubmitCodeController {
     }
 
 
-    @PostMapping("v5")
-    public String Runit2(@RequestParam(name = "lang") String language,@RequestParam(name = "code") String ClientCode,@RequestParam(name = "in") String input,@RequestParam(name = "ref") String reference) throws IOException {
-        return CodeHandlerV2.judgeWithoutSsh(language,ClientCode,input,reference);
 
-    }
     @PostMapping("v6")
     public String Runit3(@RequestParam(name = "lang") String language,@RequestParam(name = "code") String ClientCode,@RequestParam(name = "in") String input,@RequestParam(name = "ref") String reference) throws IOException {
         return CodeHandlerV3.judgeWithoutSshV2(language,ClientCode,input,reference);
